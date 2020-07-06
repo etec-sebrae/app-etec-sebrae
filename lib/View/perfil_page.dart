3f2de8116a3d922123a6417ed46eea3da6871dbe
jaelson91 <61023298+jaelson91@users.noreply.github.com>
2020-07-06 01:34:27 -0300

    puxando dados api aluno

--- lib/View/perfil_page.dart
+++ lib/View/perfil_page.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:http/http.dart' as http;
 
 class PerfilPage extends StatefulWidget {
   @override
@@ -12,8 +13,27 @@ class _PerfilPageState extends State<PerfilPage> {
   Color corInicioGradiente = const Color(0xf3223249);
   Color corFinalGradiente = const Color(0xff5165CB);
   Color corContainer = const Color(0xffffffff);
+
+  String _nomealunoperfil = "";
+  String _matriculaperfil = "";
+  String _emailperfil = "";
+  String _cursoperfil = "";
+
+  Future<void> _dadosaluno() async {
+    String url = "https://seetecc.herokuapp.com/api/aluno/1";
+    http.Response response = await http.get(url);
+    setState(() {
+      _nomealunoperfil = jsonDecode(response.body)['data']['nome'].toString();
+      _matriculaperfil = jsonDecode(response.body)['data']['matricula'].toString();
+      _emailperfil = jsonDecode(response.body)['data']['login']['email'].toString();
+      _cursoperfil = jsonDecode(response.body)['data']['curso']['nome'].toString();
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    _dadosaluno();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dados Pessoais'),
@@ -31,45 +51,41 @@ class _PerfilPageState extends State<PerfilPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
-              backgroundImage: AssetImage('assets/profile.png'),
+              //backgroundImage: AssetImage('assets/profile.png'),
               radius: 60,
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                style: TextStyle(fontSize: 16.0),
-                decoration: InputDecoration(
-                    labelText: 'Nome:'
-                ),
+
+              child: Text(" ${_nomealunoperfil}", style: TextStyle(fontSize: 24),
+
+              ) ,
+            ),
+
+            Card(
+              child: ListTile(
+
+                title: Text('RA:', style: TextStyle(fontSize: 12), ),
+                subtitle: Text(_matriculaperfil,  style: TextStyle(fontSize: 18),),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                style: TextStyle(fontSize: 16.0),
-                decoration: InputDecoration(
-                    labelText: 'RA:'
-                ),
+
+            Card(
+              child: ListTile(
+
+                title: Text('E-Mail:', style: TextStyle(fontSize: 12), ),
+                subtitle: Text(_emailperfil,  style: TextStyle(fontSize: 18),),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                style: TextStyle(fontSize: 16.0),
-                decoration: InputDecoration(
-                    labelText: 'E-mail:'
-                ),
+            Card(
+              child: ListTile(
+
+                title: Text('Curso:', style: TextStyle(fontSize: 12), ),
+                subtitle: Text(_cursoperfil,  style: TextStyle(fontSize: 18),),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                style: TextStyle(fontSize: 16.0),
-                decoration: InputDecoration(
-                    labelText: 'Data Nascimento:'
-                ),
-              ),
-            ),
+
+
 
           ],
         ),
