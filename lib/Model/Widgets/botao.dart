cde501dff1bb95a2aec2a4fe8ecbaab91480ff50
Leonardo Salazar <leonardofaria_7@hotmail.com>
2020-07-08 23:06:49 -0300

    Ajustando quebra dos botões

--- lib/Model/Widgets/botao.dart
+++ lib/Model/Widgets/botao.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-Widget construirBotao(Color cor, String texto,StatefulWidget paginaDoc,BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Padding(
+Widget construirBotao(
+    Color cor, String texto, StatefulWidget paginaDoc, BuildContext context) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: <Widget>[
+      Expanded(
+        child: Container(
           padding: EdgeInsets.all(10.0),
           child: FlatButton(
             shape: RoundedRectangleBorder(
@@ -13,9 +15,10 @@ Widget construirBotao(Color cor, String texto,StatefulWidget paginaDoc,BuildCont
             color: cor,
             child: Stack(
               children: <Widget>[
-                SizedBox(
+                Container(
+                  
                   height: 80,
-                  width: 350,
+                  width: 380,
                   child: Padding(
                     padding: EdgeInsetsDirectional.fromSTEB(0.0, 35, 0.0, 0.0),
                     child: Text(
@@ -26,13 +29,16 @@ Widget construirBotao(Color cor, String texto,StatefulWidget paginaDoc,BuildCont
                   ),
                 ),
                 Padding(
-                    padding: EdgeInsets.fromLTRB(323.0, 25, 0.0, 0.0),
-                    child: SvgPicture.asset(
-                      'assets/papel.svg',
-                      color: Colors.white,
-                      width: 30,
-                      height: 30,
-                    ))
+                  padding: EdgeInsets.only(top:25.0),
+                  child: Align(
+                      alignment: Alignment.centerRight,
+                      child: SvgPicture.asset(
+                        'assets/papel.svg',
+                        color: Colors.white,
+                        width: 30,
+                        height: 30,
+                      )),
+                )
               ],
             ),
             onPressed: () {
@@ -43,6 +49,7 @@ Widget construirBotao(Color cor, String texto,StatefulWidget paginaDoc,BuildCont
             },
           ),
         ),
-      ],
-    );
-  }
+      ),
+    ],
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:seetec_projeto/View/login_page.dart';
+import 'package:seetec_projeto/View/main_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
